7cf548097831359520c314c1df1ca0e868c096a4
Rian Negreiros <riannegreiros@gmail.com>
2023-11-16 12:35:56 -0300

    feat: handle publish and consume

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"sync"
@@ -25,6 +26,10 @@ type Server struct {
 }
 
 func main() {
+	server := &Server{
+		Topics: make(map[string]*Topic),
+	}
+
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
@@ -33,4 +38,81 @@ func main() {
 	defer listener.Close()
 
 	fmt.Println("Server is listening on localhost:8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn, server)
+	}
+}
+
+func handleConnection(conn net.Conn, server *Server) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		command := scanner.Text()
+
+		switch command {
+		case "PUBLISH":
+			handlePublish(conn, server)
+		case "CONSUME":
+			handleConsume(conn, server)
+		default:
+			fmt.Fprintln(conn, "Unknown command:", command)
+		}
+	}
+}
+
+func handlePublish(conn net.Conn, server *Server) {
+	fmt.Fprintln(conn, "Enter topic name:")
+	scanner := bufio.NewScanner(conn)
+	scanner.Scan()
+	topicName := scanner.Text()
+
+	server.mu.Lock()
+	topic, ok := server.Topics[topicName]
+	if !ok {
+		topic = &Topic{Name: topicName}
+		server.Topics[topicName] = topic
+	}
+	server.mu.Unlock()
+
+	fmt.Fprintln(conn, "Enter message content:")
+	scanner.Scan()
+	messageContent := scanner.Text()
+
+	topic.mu.Lock()
+	topic.Messages = append(topic.Messages, Message{Content: messageContent})
+	topic.mu.Unlock()
+
+	fmt.Fprintln(conn, "Message published successfully.")
+}
+
+func handleConsume(conn net.Conn, server *Server) {
+	fmt.Fprintln(conn, "Enter topic name:")
+	scanner := bufio.NewScanner(conn)
+	scanner.Scan()
+	topicName := scanner.Text()
+
+	server.mu.Lock()
+	topic, ok := server.Topics[topicName]
+	if !ok {
+		server.mu.Unlock()
+		fmt.Fprintln(conn, "Topic not found.")
+		return
+	}
+	server.mu.Unlock()
+
+	topic.mu.Lock()
+	for _, message := range topic.Messages {
+		fmt.Fprintln(conn, message.Content)
+	}
+	topic.mu.Unlock()
+
+	fmt.Fprintln(conn, "Waiting for new messages...")
 }
