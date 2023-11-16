503baed257f0d4c07ae3d5730aa0b07feeb6e1a6
Rian Negreiros <riannegreiros@gmail.com>
2023-11-16 11:21:13 -0300

    Setup basic tcp server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RianNegreiros/devgym-clone-kafka
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on localhost:8080")
+}
